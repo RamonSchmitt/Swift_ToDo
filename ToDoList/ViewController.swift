26e8b6f16cffbfa1c7d25b157de26b33774752d4
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-10-28 15:22:21 +0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Ramon Schmitt on 28/10/2016.
+//  Copyright © 2016 Ramon Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
